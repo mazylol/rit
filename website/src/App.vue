38180013514135e810329af349aed2458e5ecb2a
Mazy <landonp95@gmail.com>
2022-06-20 00:57:48 -0400

    website

---
+++ website/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="min-h-screen bg-neutral-700">
+    <div class="pt-32">
+      <p class="text-9xl text-gray-200 text-center font-extrabold">Rit</p>
+      <p class="pt-2 font-bold text-center text-2xl text-gray-200">
+        The Rusty Git Wrapper
+      </p>
+    </div>
+    <div class="pt-32 text-center text-gray-200">
+      <p class="font-bold text-5xl">
+        Install:
+        <span class="p-1 bg-neutral-800 rounded-md"
+          >cargo install rit-mazy</span
+        >
+      </p>
+    </div>
+  </div>
+</template>
